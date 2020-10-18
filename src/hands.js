ae979a0b37b3a1c3a2f2815cfee554452fd37c6f
Jared Rooprai <hello@jaredrooprai.com>
2020-10-17 18:22:45 -0600

    test

--- src/hands.js
+++ src/hands.js
@@ -5,16 +5,18 @@ import axios from 'axios';
 
 const Hands = () => {
 
-    const { data } = useQuery('todos', axios.get(`/.netlify/functions/hello`))
 
     const click = () => {
         console.log('test')
     }
 
-    useEffect(() => {console.log(data)},[data])
+    useEffect(() => {
+        axios.get(`/.netlify/functions/hello`).then((resp) => {
+            console.log(resp)
+        })
+    },[])
     return (
         <>
-            <p>{data && data.message}</p>
             <button onClick={click}>Click me</button>
         </>
     )
