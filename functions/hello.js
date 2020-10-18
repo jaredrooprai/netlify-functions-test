de5986f29df41001663a7b54b5ca606ca7373d4e
Jared Rooprai <hello@jaredrooprai.com>
2020-10-17 18:07:38 -0600

    fix body test thingy

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
-        data: JSON.stringify({message: "Hello World"})
+        body: JSON.stringify({message: "Hello World"})
     };
 }
